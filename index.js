2ebd9757081a52089cf7c21a5ed8e43ec94f8e40
Mursaleen Taqi <leemursa@gmail.com>
2019-12-10 06:48:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-// Write your code here
-class Breakfast{
-  constructor(food, drink){
-    this.food=food;
-    this.drink=drink;
+
+class Breakfast {
+  constructor(food, drink) {
+    this.food = food
+    this.drink = drink
   }
 }
 
-let breakfast1=new Breakfast(bread, tea);
+class Lunch {
+  constructor(salad, soup, drink) {
+    this.salad = salad
+    this.soup = soup
+    this.drink = drink
+  }
+}
+
+class Dinner {
+  constructor(salad, soup, entree, dessert) {
+    this.salad = salad 
+    this.soup = soup 
+    this.entree = entree 
+    this._dessert = dessert
+  }
+}
